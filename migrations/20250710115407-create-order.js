a0dfd9ac0e4cf9129ba3fb0faf3ed06cd2ce2935
nabeelnaeem <nnabeel03@gmail.com>
2025-07-10 16:58:17 +0500

    Order model and migration

---
+++ migrations/20250710115407-create-order.js
@@ -0,0 +1,40 @@
+/** @type {import('sequelize-cli').Migration} */
+export default {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Orders', {
+      order_id: {
+        allowNull: false,
+        primaryKey: true,
+        type: Sequelize.UUID,
+        defaultValue: Sequelize.UUIDV4
+      },
+      user_id: {
+        type: Sequelize.UUID,
+        allowNull: false,
+        unique: true,
+      },
+      date: {
+        type: Sequelize.DATE,
+        allowNull: false
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.fn('NOW')
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.fn('NOW')
+      },
+      deletedAt: {
+        type: Sequelize.DATE,
+        allowNull: true
+      }
+    });
+  },
+
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Orders');
+  }
+};
---
+++ models/order.js
@@ -0,0 +1,31 @@
+import { Model, DataTypes } from "sequelize";
+
+export default (sequelize) => {
+  class Order extends Model {
+    static associate(models) {
+
+    }
+  }
+  Order.init({
+    order_id: {
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4,
+      primaryKey: true
+    },
+    user_id: {
+      type: DataTypes.UUID,
+      allowNull: false,
+    },
+    date: {
+      type: DataTypes.DATE,
+      allowNull: true,
+    }
+  }, {
+    sequelize,
+    modelName: 'Order',
+    tableName: 'Orders',
+    timestamps: true,
+    paranoid: true
+  });
+  return Category;
+}
