1a583cba0fd270e739942e387055c487f43cd0f8
nabeelnaeem <nnabeel03@gmail.com>
2025-07-10 16:43:09 +0500

    Category model and migrations

---
+++ migrations/20250710113448-create-category.js
@@ -0,0 +1,40 @@
+/** @type {import('sequelize-cli').Migration} */
+export default {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Categories', {
+      category_id: {
+        allowNull: false,
+        primaryKey: true,
+        type: Sequelize.UUID,
+        defaultValue: Sequelize.UUIDV4
+      },
+      name: {
+        type: Sequelize.UUID,
+        allowNull: false,
+        unique: true,
+      },
+      description: {
+        type: Sequelize.TEXT,
+        allowNull: false
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.fn('NOW')
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.fn('NOW')
+      },
+      deletedAt: {
+        type: Sequelize.DATE,
+        allowNull: true
+      }
+    });
+  },
+
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Categories');
+  }
+};
---
+++ models/category.js
@@ -0,0 +1,31 @@
+import { Model, DataTypes } from "sequelize";
+
+export default (sequelize) => {
+  class Category extends Model {
+    static associate(models) {
+
+    }
+  }
+  Category.init({
+    category_id: {
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4,
+      primaryKey: true
+    },
+    name: {
+      type: DataTypes.UUID,
+      allowNull: false,
+    },
+    description: {
+      type: DataTypes.TEXT,
+      allowNull: true,
+    }
+  }, {
+    sequelize,
+    modelName: 'Category',
+    tableName: 'Categories',
+    timestamps: true,
+    paranoid: true
+  });
+  return Category;
+}
