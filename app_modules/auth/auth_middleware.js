644ae5aab301ea83a77f8c212ca6827bf6f79a23
nabeelnaeem <nnabeel03@gmail.com>
2025-07-10 12:45:17 +0500

    fix constants in auth_controller

--- app_modules/auth/auth_middleware.js
+++ app_modules/auth/auth_middleware.js
@@ -1,20 +1,24 @@
 import jwt from 'jsonwebtoken';
+const TOKEN_MISSING_MESSAGE = 'Token missing';
+const NO_TOKEN_PROVIDED_MESSAGE = 'No token Provided';
+const INVALID_TOKEN_MESSAGE = 'Invalid or expired token';
+
 
 export const verifyToken = (req, res, next) => {
     const authHeader = req.headers['authorization'];
 
     if (!authHeader)
-        return res.status(401).json({ error: 'No token Provided' });
+        return res.status(401).json({ error: NO_TOKEN_PROVIDED_MESSAGE });
 
     //"Bearer <token>"
     const token = authHeader.split(' ')[1];
 
     if (!token)
-        return res.status(401).json({ error: 'Token missing' });
+        return res.status(401).json({ error: TOKEN_MISSING_MESSAGE });
 
     jwt.verify(token, 'abcd', (err, decoded) => {
         if (err)
-            return res.status(403).json({ error: 'Invalid or expired token' });
+            return res.status(403).json({ error: INVALID_TOKEN_MESSAGE });
         req.user = decoded;
         next();
     });
