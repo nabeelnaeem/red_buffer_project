2e44386b3abb6bd07abeac3439193a59278b9f00
nabeelnaeem <nnabeel03@gmail.com>
2025-07-14 14:07:04 +0500

    Updated product_route.js

--- app_modules/product/product_route.js
+++ app_modules/product/product_route.js
@@ -1,11 +1,12 @@
 import express from 'express';
+import { createProduct, getAllProducts, getProductById, updateProduct, deleteProduct } from "./product_controller.js"
 
 const router = express.Router();
 
-router.post('/');
-router.get('/');
-router.get('/:id');
-router.post('/:id');
-router.delete('/:id');
+router.post('/', createProduct);
+router.get('/', getAllProducts);
+router.get('/:id', getProductById);
+router.put('/:id', updateProduct);
+router.delete('/:id', deleteProduct);
 
 export default router;
