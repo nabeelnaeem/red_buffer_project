f3a0506f774fa05d7e4ce1ac56187508436fe555
nabeelnaeem <nnabeel03@gmail.com>
2025-07-18 12:40:15 +0500

    updated routes, added endpoint to update profile

--- app_modules/auth/auth_route.js
+++ app_modules/auth/auth_route.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { signup, login, revoke, profile } from './auth_controller.js';
+import { signup, login, revoke, profile, updateProfile } from './auth_controller.js';
 import { verifyToken } from './auth_middleware.js';
 
 const router = express.Router();
@@ -9,4 +9,5 @@ router.put('/revoke', verifyToken, revoke);
 
 //protected route
 router.get('/profile', verifyToken, profile);
+router.put('/profile', verifyToken, updateProfile);
 export default router;
