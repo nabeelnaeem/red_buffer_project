0911b5042db68b5efd56c35a3ae7615b5b815642
nabeelnaeem <nnabeel03@gmail.com>
2025-07-23 13:16:43 +0500

    Implemented create and update product review

--- app_modules/review/review_controller.js
+++ app_modules/review/review_controller.js
@@ -1,10 +1,11 @@
-import { hasPurchasedProduct } from "./services/review_service.js"
+import { hasPurchasedProduct, createProductReview, updateProductReview, getReviewIfAny } from "./services/review_service.js"
 import { getUserIdFromToken } from '../auth/services/auth_service.js';
 
 // Error messages
 const UNAUTHORIZED_ERROR = 'Unauthorized: user ID missing';
 const PRODUCT_ID_REQUIRED_ERROR = 'Product ID is required';
 const PURCHASE_CHECK_ERROR = 'Failed to check purchase status';
+const PRODUCT_RATING_REQUIRED_MSG = 'product_id and rating are required';
 
 export const checkIfUserPurchasedProduct = async (req, res) => {
     try {
@@ -20,9 +21,59 @@ export const checkIfUserPurchasedProduct = async (req, res) => {
         }
 
         const hasPurchased = await hasPurchasedProduct(user_id, product_id);
-        return res.status(200).json({ hasPurchased })
+        let review = null;
+        if (hasPurchased) {
+            review = await getReviewIfAny(user_id, product_id)
+        }
+
+        return res.status(200).json({ hasPurchased, review })
     }
     catch (error) {
         return res.status(500).json({ error: PURCHASE_CHECK_ERROR });
+    }
+};
+
+export const createReview = async (req, res) => {
+    const { product_id, rating, comment, order_item_id } = req.body;
+    const user_id = getUserIdFromToken(req);
+
+    if (!product_id || !rating) {
+        return res.status(400).json({ error: PRODUCT_RATING_REQUIRED_MSG });
+    }
+    try {
+        const review = await createProductReview(user_id, product_id, order_item_id, rating, comment);
+        res.status(201).json({
+            message: 'Review submitted',
+            review: {
+                rating: review.rating,
+                comment: review.comment
+            }
+        });
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({ error: err.message });
+    }
+};
+
+export const updateReview = async (req, res) => {
+    const { product_id, rating, comment } = req.body;
+    const user_id = getUserIdFromToken(req);
+
+    if (!product_id || !rating) {
+        return res.status(400).json({ error: PRODUCT_RATING_REQUIRED_MSG });
+    }
+
+    try {
+        const review = await updateProductReview(user_id, product_id, rating, comment);
+        return res.status(200).json({
+            message: 'Review updated successfully',
+            review: {
+                rating: review.rating,
+                comment: review.comment
+            }
+        });
+    } catch (err) {
+        console.error('Update Review Error:', err.message);
+        return res.status(400).json({ error: err.message });
     }
 };
--- app_modules/review/services/review_service.js
+++ app_modules/review/services/review_service.js
@@ -13,3 +13,78 @@ export const hasPurchasedProduct = async (user_id, product_id) => {
     });
     return result.length > 0;
 };
+
+export const getReviewIfAny = async (user_id, product_id) => {
+    const [reviewResult] = await sequelize.query(`
+            SELECT rating, comment FROM reviews
+            WHERE user_id = :user_id AND product_id = :product_id
+            LIMIT 1
+        `, {
+        replacements: { user_id, product_id }
+    });
+
+    if (reviewResult.length > 0) {
+        return reviewResult[0];
+    }
+    else {
+        return null;
+    }
+}
+
+export const createProductReview = async (user_id, product_id, order_item_id, rating, comment) => {
+    try {
+        const [existingReview] = await sequelize.query(`
+            SELECT review_id FROM reviews
+            WHERE user_id = :user_id AND product_id = :product_id
+        `, {
+            replacements: { user_id, product_id }
+        });
+
+        if (existingReview.length > 0) {
+            throw new Error('You have already reviewed this product');
+        }
+
+        if (!order_item_id) {
+            const [rows] = await sequelize.query(`
+                SELECT oi.order_item_id
+                FROM order_item oi
+                JOIN orders o ON o.order_id = oi.order_id
+                WHERE oi.product_id = :product_id AND o.user_id = :user_id
+                ORDER BY oi."createdAt" DESC
+                LIMIT 1;
+            `, { replacements: { user_id, product_id } });
+
+            if (!rows.length) throw new Error('No recent purchase found for this product');
+
+            order_item_id = rows[0].order_item_id;
+        }
+        const [result] = await sequelize.query(`
+            INSERT INTO reviews (review_id, user_id, product_id, order_item_id, rating, comment, "createdAt", "updatedAt")
+            VALUES (gen_random_uuid(), :user_id, :product_id, :order_item_id, :rating, :comment, NOW(), NOW())
+            RETURNING *;
+        `, {
+            replacements: { user_id, product_id, order_item_id, rating, comment }
+        });
+
+        return result[0];
+    } catch (err) {
+        throw err;
+    }
+};
+
+export const updateProductReview = async (user_id, product_id, rating, comment) => {
+    const [result] = await sequelize.query(`
+        UPDATE reviews
+        SET rating = :rating,
+            comment = :comment,
+            "updatedAt" = NOW()
+        WHERE user_id = :user_id AND product_id = :product_id
+        RETURNING *;
+    `, {
+        replacements: { user_id, product_id, rating, comment }
+    });
+
+    if (!result.length) throw new Error('Review not found to update');
+
+    return result[0];
+};
