928a7eb20b1155ba478ce1951ac9f5cfdb7b7258
nabeelnaeem <nnabeel03@gmail.com>
2025-07-24 12:37:07 +0500

    Update auth_routes, added refresh-token, logout routes

--- app_modules/auth/auth_route.js
+++ app_modules/auth/auth_route.js
@@ -1,13 +1,13 @@
 import express from 'express';
-import { signup, login, revoke, profile, updateProfile } from './auth_controller.js';
+import { signup, login, revoke, profile, updateProfile, refreshAccessToken, logout } from './auth_controller.js';
 import { verifyToken } from './auth_middleware.js';
 
 const router = express.Router();
 router.post('/signup', signup);
 router.post('/login', login);
+router.post('/refresh-token', verifyToken, refreshAccessToken);
+router.post('/logout', verifyToken, logout);
 router.put('/revoke', verifyToken, revoke);
-
-//protected route
 router.get('/profile', verifyToken, profile);
 router.put('/profile', verifyToken, updateProfile);
 export default router;
